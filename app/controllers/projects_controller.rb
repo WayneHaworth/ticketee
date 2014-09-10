dbd1f78785b7af7729ff34194aabae8dabdba6b3
Wayne Haworth <waynehaworth@gmail.com>
2014-09-10 21:27:59 +0100

    Add validations for Project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,8 @@ class ProjectsController < ApplicationController
     end
 
     def new
-        @project = Project.new
+        @project = Project.new(project_params)
+        @project.save
     end
 
     def create
@@ -13,7 +14,8 @@ class ProjectsController < ApplicationController
             flash[:notice] = "Project has been created."
             redirect_to @project
         else
-            #nothing yet
+            flash[:alert] = "Project has not been created."
+            render "new"
         end
     end
 
